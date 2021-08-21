40f94ed25ef500fcb88de58f259f733cf63a29e1
f12-teacher <coding-school-f12@mail.ru>
2021-08-22 00:01:06 +0600

    lms1

---
+++ src/App.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class App {
+    // LMS1 - первый урок, тут только ототбражение информации в консольке
+    public static void main(String[] args) {
+        System.out.println("Welcome to LMS!");
+        System.out.println("Choose the command:");
+        System.out.println("1 - add the book");
+        System.out.println("2 - delete the book");
+        System.out.println("3 - rename the book");
+        System.out.println("4 - find the book");
+        System.out.println("5 - book's list");
+        System.out.println("6 - exit");
+
+    }
+}
